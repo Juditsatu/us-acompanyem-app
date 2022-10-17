5e9a685d503cce90f265fd3eafe0f43f5cb1cbc4
Ana Sanz <larasanz11@gmail.com>
2022-10-17 11:50:43 +0200

    routing

--- src/app/data/data-routing.module.ts
+++ src/app/data/data-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { MapComponent } from './pages/map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      { path:'home', component: HomeComponent},
+      { path:'chart', component: ChartsComponent },
+      { path:'map', component:MapComponent },
+      { path:'**', redirectTo:'home '},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
