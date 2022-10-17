a4e124869bf2dd566bd9c220688b71115761e386
Judit Santos <judit.sant@gmail.com>
2022-10-17 16:27:12 +0200

    chart data update

--- src/app/data/pages/charts/charts.component.ts
+++ src/app/data/pages/charts/charts.component.ts
@@ -1,45 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartData, ChartType } from 'chart.js';
+import { BarriService } from '../../services/barri.service';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ChartsComponent implements OnInit {
 
-// Doughnut
-public doughnutChartLabels: string[] = [
-  'Download Sales',
-  'In-Store Sales',
-  'Mail-Order Sales',
-];
-public doughnutChartData: ChartData<'doughnut'> = {
-  labels: this.doughnutChartLabels,
-  datasets: [
-    {
-      data: [350, 450, 100],
-      backgroundColor: '#FF5F5D',
-      hoverBackgroundColor: '#FF9933',
-    },
-    {
-      data: [50, 150, 120],
-      backgroundColor: '#3F7C85',
-      hoverBackgroundColor: '#0099DD',
-    },
-    {
-      data: [250, 130, 70],
-      backgroundColor: '#00CCBF',
-      hoverBackgroundColor: '#00ABBD',
-    },
-  ],
-};
-public doughnutChartType: ChartType = 'doughnut';
+  constructor( private barriService: BarriService ) {}
 
-  constructor() { }
+  public title: string = this.barriService.barris[0].nombre
 
-  ngOnInit(): void {
-  }
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    'Índex de pobresa',
+    'Major de 65 anys',
+    'Major de 65 anys que viuen sols',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [
+          this.barriService.barris[0].indicePobreza,
+          this.barriService.barris[0].mayor65,
+          this.barriService.barris[0].soledad65],
+        backgroundColor: '#FF5F5D',
+        hoverBackgroundColor: '#FF9933',
+      },
+      // {
+      //   data: [
+      //     this.barriService.barris[1].indicePobreza,
+      //     this.barriService.barris[2].mayor65,
+      //     this.barriService.barris[3].soledad65],
+      //   backgroundColor: '#3F7C85',
+      //   hoverBackgroundColor: '#0099DD',
+      // },
+      // {
+      //   data: [
+      //     this.barriService.barris[2].indicePobreza,
+      //     this.barriService.barris[2].mayor65,
+      //     this.barriService.barris[2].soledad65],
+      //   backgroundColor: '#00CCBF',
+      //   hoverBackgroundColor: '#00ABBD',
+      // },
+    ],
+  };
 
+  public doughnutChartType: ChartType = 'doughnut';
+
+  ngOnInit(): void {}
 }
