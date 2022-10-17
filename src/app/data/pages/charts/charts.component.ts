5ec4ecc767621e5c3b8c7aa3f0836c89b1b3e96f
Judit Santos <judit.sant@gmail.com>
2022-10-17 16:48:50 +0200

    chart update

--- src/app/data/pages/charts/charts.component.ts
+++ src/app/data/pages/charts/charts.component.ts
@@ -11,7 +11,9 @@ export class ChartsComponent implements OnInit {
 
   constructor( private barriService: BarriService ) {}
 
-  public title: string = this.barriService.barris[0].nombre
+  public title1: string = this.barriService.barris[0].nombre
+  public title2: string = this.barriService.barris[1].nombre
+  public title3: string = this.barriService.barris[2].nombre
 
   // Doughnut
   public doughnutChartLabels: string[] = [
@@ -30,22 +32,22 @@ export class ChartsComponent implements OnInit {
         backgroundColor: '#FF5F5D',
         hoverBackgroundColor: '#FF9933',
       },
-      // {
-      //   data: [
-      //     this.barriService.barris[1].indicePobreza,
-      //     this.barriService.barris[2].mayor65,
-      //     this.barriService.barris[3].soledad65],
-      //   backgroundColor: '#3F7C85',
-      //   hoverBackgroundColor: '#0099DD',
-      // },
-      // {
-      //   data: [
-      //     this.barriService.barris[2].indicePobreza,
-      //     this.barriService.barris[2].mayor65,
-      //     this.barriService.barris[2].soledad65],
-      //   backgroundColor: '#00CCBF',
-      //   hoverBackgroundColor: '#00ABBD',
-      // },
+      {
+        data: [
+          this.barriService.barris[1].indicePobreza,
+          this.barriService.barris[2].mayor65,
+          this.barriService.barris[3].soledad65],
+        backgroundColor: '#3F7C85',
+        hoverBackgroundColor: '#0099DD',
+      },
+      {
+        data: [
+          this.barriService.barris[2].indicePobreza,
+          this.barriService.barris[2].mayor65,
+          this.barriService.barris[2].soledad65],
+        backgroundColor: '#00CCBF',
+        hoverBackgroundColor: '#00ABBD',
+      },
     ],
   };
 
